469a17f5ac59afc6928082039fde7cdacf2df2ba
edithntinyari <emothegi@gmail.com>
2020-07-29 14:32:22 +0300

    added changes

---
+++ app/src/androidTest/java/RestaurantsActivityTest.java
@@ -0,0 +1,40 @@
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.moringaschool.myrestaurants.R;
+import com.moringaschool.myrestaurants.RestaurantsActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+public class RestaurantsActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<RestaurantsActivity> activityTestRule =
+            new ActivityTestRule<>(RestaurantsActivity.class);
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String restaurantName = "Mi Mero Mole";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(restaurantName)));
+    }
+}
